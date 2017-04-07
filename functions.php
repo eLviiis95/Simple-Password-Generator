03820aa45923aeff389372259388dd38821d0ef9
eLviis95 <office@gigagaming.de>
2017-04-07 09:50:30 +0200

    password generator

--- functions.php
+++ functions.php
@@ -1,63 +1,27 @@
 <?php
 /*
 
-Author: Elvis Halilovic
+Author: eLviiis95
 STAND: 07.04.2017
 Project: ---
 
 */
+
 function generatePassword($var) {
-	$maxLength = 2560;
-	if(empty($var)){
-		$error = "Without a length no password can be generated!<br>";
-		return $error;
-	}else{
-		if(is_numeric($var)){
-			if($var <= $maxLength){
-				$codeLength = $var;
-				$chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-=+;:,.?";
-				$password = substr(str_shuffle($chars), 0, $codeLength);
-				return $password;
-			}else{
-				$error = "<h2>The password can have a maximum of $maxLength characters. Please adjust the transferred value!</h2><br><h3>Current transfer: $var</h3>";
-				return $error;				
-			}
+	// Max password size
+	$maxLength = 256;
+	if(is_numeric($var)){
+		if($var <= $maxLength){
+			$chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-=+;:,.?";
+			$password = substr(str_shuffle($chars), 0, $var);
+			return $password;
 		}else{
-			$error = "<h2>Impossible length! Please just pass numbers and no other characters!</h2><br><h3>Current transfer: $var</h3>";
-			return $error;			
+			$error = "<h2>The password can have a maximum of $maxLength characters. Please adjust the transferred value!</h2><br><h3>Current transfer: $var</h3>";
+			return $error;				
 		}
-	}
+	}else{
+		$error = "<h2>Impossible length! Please just pass numbers and no other characters!</h2><br><h3>Current transfer: $var</h3>";
+		return $error;			
+		}
 	return $password;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
